1471b2cc72b3e62d4c3ff07dc54adcb4bf67c9c0
Suhani Ahirwar <suhaniahirwar96@gmail.com>
2026-02-10 17:44:37 +0530

    Backend setup with Express server

---
+++ Backend/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.listen(3000,(req,res)=>{
+    console.log("Server is running on port 3000");
+});
