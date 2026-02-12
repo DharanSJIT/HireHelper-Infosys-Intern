7725ec5106dd89d3665050018a4742fa07415ea5
Suhani Ahirwar <suhaniahirwar96@gmail.com>
2026-02-12 16:51:45 +0530

    sendOtp logic

--- Backend/utils/sendOtp.js
+++ Backend/utils/sendOtp.js
@@ -11,7 +11,7 @@ exports.sendOtp = async (email, otp) => {
 
   await transporter.sendMail({
     from: process.env.EMAIL,
-    to: "harshitahirwar25@gmail.com",
+    to: "",
     subject: "HireHelper OTP Verification",
     text: `Your OTP is ${otp}`
   });
