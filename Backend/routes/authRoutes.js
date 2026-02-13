f4b6b2af98b420a685afa1af82d6d5a80f79ba8b
Suhani Ahirwar <suhaniahirwar96@gmail.com>
2026-02-13 17:58:55 +0530

    resend otp feature

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddlewares");
 
-const {register,verifyOtp,login,getProfile} = require("../Controllers/authController");
+const {register,verifyOtp,login,getProfile,resendOtp} = require("../Controllers/authController");
 
 router.post("/register",register);
 router.post("/verify-otp", verifyOtp);
 router.post("/login", login);
 router.get("/profile", authMiddleware, getProfile);
+router.post("/resend-otp", resendOtp);
 
 module.exports = router;
