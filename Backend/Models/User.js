6494a47c82cd790461ec0b4cebb3aca30e0b705e
DharanSJIT <dharan.mj2k@gmail.com>
2026-03-26 13:45:14 +0530

    Replace UUID generation method with randomUUID from crypto for user ID

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const { v4: uuidv4 } = require("uuid");
+const { randomUUID } = require("crypto");
 
 const userSchema = new mongoose.Schema(
   {
     id: {
       type: String,
-      default: uuidv4,
+      default: () => randomUUID(),
       unique: true,
     },
 
